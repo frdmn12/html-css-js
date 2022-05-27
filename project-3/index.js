b1d1f5f763342125076efa929cdefdedbd0c20ee
bayu <bayuferdiman1@gmail.com>
2022-05-28 01:06:02 +0700

    hello

--- project-3/index.js
+++ project-3/index.js
@@ -1,1 +1,41 @@
-// test
+
+
+// Javascript program to print reverse
+// of words in a string.
+function wordReverse(str)
+{
+	var i = str.length - 1;
+	var start, end = i + 1;
+	var result = "";
+	
+	while (i >= 0)
+	{
+		if (str[i] == ' ')
+		{
+			start = i + 1;
+			while (start != end)
+				result += str[start++];
+			
+			result += ' ';
+			end = i;
+		}
+		i--;
+	}
+	start = 0;
+	
+	while (start != end)
+		result += str[start++];
+	
+	return result;
+}
+
+// Driver code
+var str = "I AM A GEEK";
+
+console.log(wordReverse(str)); 
+;
+console.log(str.length);
+
+// This code is contributed by rutvik_56
+
+
